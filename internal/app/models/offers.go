431c73d3a0bd570a074f4400e2e855f931a146c4
Pavel Mozhchil <pmozhchil@ozon.ru>
2022-07-13 11:06:20 +0300

    GetOffersByParams add compareOffer, sort.Slice, offerCollectionLimit

--- internal/app/models/offers.go
+++ internal/app/models/offers.go
@@ -1,13 +1,14 @@
 package models
 
 type Offer struct {
-	Region    string `json:"region"`
-	CpuSingle int32  `json:"cpuSingle"`
-	CpuMulti  int32  `json:"cpuMulti"`
-	Ram       int32  `json:"ram"`
-	DiskSpeed int32  `json:"diskSpeed"`
-	DiskSize  int32  `json:"diskSize"`
-	NetSpeed  int32  `json:"netSpeed"`
+	Region      string `json:"region"`
+	CpuSingle   int32  `json:"cpuSingle"`
+	CpuMulti    int32  `json:"cpuMulti"`
+	Ram         int32  `json:"ram"`
+	DiskSpeed   int32  `json:"diskSpeed"`
+	DiskSize    int32  `json:"diskSize"`
+	NetSpeed    int32  `json:"netSpeed"`
+	MinerRating int32  `json:"minerRating"`
 }
 
 type OfferCollection []*Offer
